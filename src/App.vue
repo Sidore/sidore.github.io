0d2c5f79c791d36f186d82c1f720f26ea9d9514b
Sidore <sidorenkovlad1995@gmail.com>
2018-04-01 22:06:03 +0300

    logo start

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   div#mainBlock
-    div#header
-        h1 {{message}}
-    div#mainContent
+    div#header.fluid
+        div.container
+            h1.mailLogo 
+                span.logo S
+                span idore
+    div#mainContent.fluid
         div
             p I'm a hardcore developer
         div
             p  lorem ipsum dolar
-    div#preFooter
+    div#preFooter.fluid
         p map
-    div#footer
+    div#footer.fluid
         p contacts
 </template>
 <script>
@@ -25,14 +28,37 @@ export default {
 }
 </script>
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Dancing+Script');
+
 body {
     margin: 0;
     padding: 0;
+    display: flex;
+    flex-direction: column;
 }
+
+h1 {
+    margin: 0;
+}
+
 #header {
     width: 100%;
-    padding: 30px;
     text-align: center;
+    display: flex;
+    height: 100vh;
+    background: #ccc url('../static/img/moving-through-stars-in-space_-1zccenlb__F0000.png') center center;
+    background-size: cover;
+    justify-content: center;
+}
+.container {
+    display: flex;
+    align-items: center;
+    // width: 1000px;
+    // margin: auto;
+    // background: #ccc;
+}
+.fluid {
+    width: 100%;
 }
 p {
     color: white;
@@ -40,6 +66,30 @@ p {
     padding: 15px;
     margin: 15px;
 }
+
+.mailLogo {
+    color: #eee;
+    font-size: 20vh;
+    font-family: 'Dancing Script', cursive;
+    z-index: 10;
+    .logo {
+        position: relative;
+        &::after {
+            height: 22vh;
+            width: 22vh;
+            content: '';
+            border: 1px #eee solid;
+            transform: rotate(45deg);
+            position: absolute;
+            top: 5px;
+            left: -12px;
+            z-index: 1;
+        }
+    }
+
+}
+
+
 </style>
 
 
